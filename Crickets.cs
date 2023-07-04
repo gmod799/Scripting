efe8716d935d992ba9122b618f8be3b7641d62ba
gmod799 <111252782+gmod799@users.noreply.github.com>
2023-07-04 15:28:32 +0300

    Crickets print loop
    Crickets Crickets Crickets Crickets Crickets Crickets Crickets Crickets Crickets Crickets

---
+++ Crickets.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crickets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Printing crickets 1 time
+        print("crickets");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //what
+        print("crickets");
+
+    }
+}
